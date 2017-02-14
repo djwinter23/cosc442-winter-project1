ead1b3273a4e54388f2b6b482766e6f5bdfd2452
David Winter <dwinte3@students.towson.edu>
2017-02-13 22:17:20 -0500

    Refactoring by extracting an interface
    Using the refactoring tools in eclipse, an interface called IOwnable was
    extracted from the Cell.java class. The methods getName, getTheOwner,
    getPrice, setTheOwner, isAvailable, and setAvailable were also extracted
    from the Cell.java class and sent to the IOwnable interface.

---
+++ Monopoly/src/edu/towson/cis/cosc442/project1/monopoly/IOwnable.java
@@ -0,0 +1,17 @@
+package edu.towson.cis.cosc442.project1.monopoly;
+
+public interface IOwnable {
+
+	String getName();
+
+	Player getTheOwner();
+
+	int getPrice();
+
+	void setTheOwner(Player owner);
+
+	boolean isAvailable();
+
+	void setAvailable(boolean available);
+
+}
